e794d28624f7b53a7f30e52b76e4f9469f0910f4
Douglas Schultz <vxpx17@gmail.com>
2018-04-19 17:13:07 -0400

    scaffolded project, added data models to correspond with existing financial planner db. Updated identity models and webApi controller to enable consumption of db resources. glued identity models to stored procedures.

---
+++ FinPlanWebAPi/FinPlanWebAPi/Controllers/DataController.cs
@@ -0,0 +1,94 @@
+﻿using FinPlanWebAPi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using static FinPlanWebAPi.Models.DataModels;
+
+namespace FinPlanWebAPi.Controllers
+{
+    [RoutePrefix("api/$Planner")]
+    public class DataController : ApiController
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        // Get methods
+
+        [Route("Accounts")]
+        public async Task<List<PersonalAccount>> GetAccountByHouseholdId(int HouseholdId)
+        {
+            return await db.GetAccountByHouseholdId(HouseholdId);
+        }
+
+        [Route("AccountDetail")]
+        public async Task<PersonalAccount> GetPersonalAccount(int AccountId)
+        {
+            return await db.GetAccountDetail(AccountId);
+        }
+
+        [Route("BudgetDetail")]
+        public async Task<Budget> GetBudgetDetails(int BudgetId)
+        {
+            return await db.GetBudgetDetails(BudgetId);
+        }
+
+        [Route("Budgets")]
+        public async Task<List<Budget>> GetBudgets(int HouseholdId)
+        {
+            return await db.GetBudgets(HouseholdId);
+        }
+
+        [Route("Household")]
+        public async Task<Household> GetHousehold(int HouseholdId)
+        {
+            return await db.GetHousehold(HouseholdId);
+        }
+
+        [Route("Transaction")]
+        public async Task<Transaction> GetTransactionDetail(int TransactionId)
+        {
+            return await db.GetTransactionDetail(TransactionId);
+        }
+
+        [Route("BudgetBalance")]
+        public async Task<string> GetBudgetBalance(int BudgetId)
+        {
+            return await db.GetBudgetBalance(BudgetId);
+        }
+
+        [Route("AccountBalance")]
+        public async Task<string> GetAccountBalance(int AccountId)
+        {
+            return await db.GetAccountBalance(AccountId);
+        }
+
+        //Post methods
+        [Route("AddTransaction")]
+        [AcceptVerbs("GET", "POST")]
+        public int AddTransaction(int accountId, string description, decimal amount, bool trxType, bool isVoid, int categoryId, string userId, bool reconciled, decimal recBalance, bool isDeleted)
+        {
+            return db.AddTransaction(accountId, description, amount, trxType, isVoid, categoryId, userId, reconciled, recBalance, isDeleted);
+        }
+
+        [Route("AddBudget")]
+        [AcceptVerbs("GET", "POST")]
+        public int AddBudget(string name, int householdId)
+        {
+            return db.AddBudget(name, householdId);
+        }
+
+        [Route("AddBudgetItem")]
+        [AcceptVerbs("GET", "POST")]
+        public int AddBudgetItem(int categoryId, int budgetId, decimal amount)
+        {
+            return db.AddBudgetItem(categoryId, budgetId, amount);
+        }
+
+        [Route("AddHousehold")]
+        [AcceptVerbs("GET", "POST")]
+        public int AddHousehold(string name)
+        {
+            return db.AddHousehold(name);
+        }
+    }
+}
+
---
+++ FinPlanWebAPi/FinPlanWebAPi/Models/IdentityModels.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using static FinPlanWebAPi.Models.DataModels;
+
+namespace FinPlanWebAPi.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        // SQL READ tasks
+
+        public async Task<List<PersonalAccount>> GetAccountByHouseholdId(int hhId)
+        {
+            return await Database.SqlQuery<PersonalAccount>("GetAllYears @hhId",
+                new SqlParameter("hhId", hhId)).ToListAsync();
+        }
+
+        public async Task<PersonalAccount> GetAccountDetail(int acctId)
+        {
+            return await Database.SqlQuery<PersonalAccount>("GetAccountDetail @acctId",
+                new SqlParameter("acctId", acctId)).FirstOrDefaultAsync();
+        }
+
+        public async Task<Budget> GetBudgetDetails(int bgtId)
+        {
+            return await Database.SqlQuery<Budget>("GetBudgetDetails @bdtId",
+                new SqlParameter("bgtId", bgtId)).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Budget>> GetBudgets(int hhId)
+        {
+            return await Database.SqlQuery<Budget>("GetBudgets @hhId",
+                new SqlParameter("hhId", hhId)).ToListAsync();
+        }
+
+        public async Task<Household> GetHousehold(int hhId)
+        {
+            return await Database.SqlQuery<Household>("GetHousehold @hhId",
+                new SqlParameter("hhId", hhId)).FirstOrDefaultAsync();
+        }
+
+        public async Task<Transaction> GetTransactionDetail(int trId)
+        {
+            return await Database.SqlQuery<Transaction>("GetTransactionDetail @trId",
+                new SqlParameter("trId", trId)).FirstOrDefaultAsync();
+        }
+
+        public async Task<string> GetBudgetBalance(int bgtId)
+        {
+            return await Database.SqlQuery<string>("GetBudgetBalance @bgtId",
+                new SqlParameter("bgtId", bgtId)).FirstOrDefaultAsync();
+        }
+
+        public async Task<string> GetAccountBalance(int AccountId)
+        {
+            return await Database.SqlQuery<string>("GetAccountBalance @AccountId",
+                new SqlParameter("AccountId", AccountId)).FirstOrDefaultAsync();
+        }
+
+        // SQL CREATE tasks
+        public int AddBudget(string name, int hhId)
+        {
+            return Database.ExecuteSqlCommand("AddBudget @name @hhId",
+                new SqlParameter("name", name),
+                new SqlParameter("hhId", hhId));
+        }
+
+        public int AddBudgetItem(int catId, int bdtId, decimal amnt)
+        {
+            return Database.ExecuteSqlCommand("AddBudgetItme @catId @bdtId @amnt",
+                new SqlParameter("catId", catId),
+                new SqlParameter("bdtId", bdtId),
+                new SqlParameter("amnt", amnt));
+        }
+
+        public int AddHousehold(string name)
+        {
+            return Database.ExecuteSqlCommand("AddHousehold @name",
+                new SqlParameter("name", name));
+        }
+
+        public int AddTransaction(int accId, string desc, decimal amt, bool typ, bool isVoid, int catId, string entById, bool recond, decimal recondAmt, bool isDel)
+        {
+            return Database.ExecuteSqlCommand("AddTransaction @accId @desc @crAt @amt @typ @isVoid @catId @entById, @recond, @recondAmt, @isDel",
+                new SqlParameter("accId", accId),
+                new SqlParameter("desc", desc),
+                new SqlParameter("crAt", DateTimeOffset.Now),
+                new SqlParameter("amt", amt),
+                new SqlParameter("typ", typ),
+                new SqlParameter("isVoid", isVoid),
+                new SqlParameter("catId", catId),
+                new SqlParameter("entById", entById),
+                new SqlParameter("recond", recond),
+                new SqlParameter("recondAmt", recondAmt),
+                new SqlParameter("isDel", isDel));
+        }
+    }
+}
